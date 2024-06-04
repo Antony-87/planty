d582990e541450960622fa97b2a204d4791020ef
Antony Reigner <antonyreigner45@gmail.com>
2024-06-04 13:21:07 +0200

    mise à jour du menu

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -4,4 +4,15 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'primary' => __('Primary Menu'),
+            'footer' => __('Footer Menu')
+        )
+    );
+}
+add_action('init', 'register_my_menus');
